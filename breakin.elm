0585e3e53f034d7d1143041e06ff4453711872ed
Tony McKinney <tony.w.mckinney@gmail.com>
2016-10-21 23:56:18 -0400

    grid with different colored blocks

--- breakin.elm
+++ breakin.elm
@@ -6,6 +6,11 @@ import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
+(=>) : a -> b -> ( a, b )
+(=>) =
+    (,)
+
+
 main =
     App.beginnerProgram
         { model = model
@@ -27,12 +32,45 @@ type alias Brick =
 
 
 type alias Model =
-    Brick
+    { bricks : List Brick }
+
+
+color i =
+    case i % 3 of
+        0 ->
+            "red"
+
+        1 ->
+            "blue"
+
+        2 ->
+            "green"
+
+        _ ->
+            "black"
+
+
+createRow seed rowNum len =
+    if len == 0 then
+        []
+    else
+        (createBrick rowNum len (color seed)) :: (createRow (rowNum * seed + len) rowNum (len - 1))
+
+
+createBrick rowNum colNum color =
+    { id = (rowNum + 1) * colNum
+    , pos = ( 5 + 5 * colNum + colNum * 40, 20 * rowNum + 5 + 5 * rowNum )
+    , size = ( 40, 20 )
+    , color = color
+    }
 
 
 model : Model
 model =
-    { id = 0, pos = ( 0, 0 ), size = ( 40, 20 ), color = "red" }
+    { bricks =
+        [0..5]
+            |> List.concatMap (\i -> createRow i i 10)
+    }
 
 
 
@@ -53,13 +91,17 @@ update msg model =
 -- VIEW
 
 
-brick : Brick -> Html Msg
-brick brick =
+renderBrick : Brick -> Html Msg
+renderBrick brick =
     div
         [ style
-            [ ( "width", toString (fst brick.size) ++ "px" )
-            , ( "height", toString (snd brick.size) ++ "px" )
-            , ( "background-color", brick.color )
+            [ "position" => "absolute"
+            , "left" => (toString (fst brick.pos) ++ "px")
+            , "top" => (toString (snd brick.pos) ++ "px")
+            , "width" => (toString (fst brick.size) ++ "px")
+            , "height" => (toString (snd brick.size) ++ "px")
+            , "background-color" => brick.color
+            , "display" => "inline-block"
             ]
         ]
         []
@@ -67,8 +109,21 @@ brick brick =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ brick model
-        , hr [] []
-        , text <| toString model
+    div
+        [ style
+            [ "background-color" => "#eee"
+            , "width" => "100%"
+            , "height" => "100%"
+            ]
+        ]
+        [ div [] (List.map renderBrick model.bricks)
+        , div
+            [ style
+                [ "position" => "absolute"
+                , "top" => "400px"
+                ]
+            ]
+            [ hr [] []
+            , text <| toString model
+            ]
         ]
