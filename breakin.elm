1c030d45ffdb20b6bf70fe554fbd71489f557241
Tony McKinney <tony.w.mckinney@gmail.com>
2016-10-31 21:26:00 -0400

    add game over screen

--- breakin.elm
+++ breakin.elm
@@ -18,33 +18,4 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { counter = 0
-      , paddle =
-            { id = Paddle
-            , x = 160
-            , y = 400
-            , sx = 100
-            , sy = 20
-            , vx = 0
-            , vy = 0
-            , color = "#ddd"
-            }
-      , bricks =
-            level1
-      , ball =
-            { id = Ball
-            , x = 210
-            , y = 380
-            , sx = 20
-            , sy = 20
-            , vx = 0
-            , vy = 0
-            , color = "#bbb"
-            }
-      , state = Title
-      , lives = 3
-      , score = 0
-      , level = 0
-      }
-    , Cmd.none
-    )
+    ( initialModel, Cmd.none )
